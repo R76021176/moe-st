b458bfe53af30e952bf94d6862f20dae46d74b02
Howard-Yu <Howard-Yu@PCC-3000002733.pcc.com.tw>
2018-08-08 15:15:09 +0800

    20180808 pm3:15 update

---
+++ maven-demo/src/test/java/fcu/sqa/maven_demo/AssessmentTest.java
@@ -0,0 +1,32 @@
+package fcu.sqa.maven_demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AssessmentTest {
+	private Assessment myAssessment = null;
+	@Before
+	public void setUp() throws Exception {
+		myAssessment = new Assessment();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetGrade() {
+		
+		String expectedGrade = "Failed";
+		String actualGrade = myAssessment.getGrade(120);
+		Assert.assertEquals(expectedGrade, actualGrade);
+		//fail("Not yet implemented");
+		
+	}
+
+}
